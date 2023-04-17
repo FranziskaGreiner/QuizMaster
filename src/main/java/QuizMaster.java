48ae926b303961611a89bfd2c7df49fd8f9ac87c
Franziska Greiner <franziska.greiner98@gmx.de>
2023-04-17 10:49:42 +0200

    Add first draft of API call

--- src/main/java/QuizMaster.java
+++ src/main/java/QuizMaster.java
@@ -1,8 +1,26 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class QuizMaster {
 
+    private static final String API_URL = "https://quizapi.io/api/v1/questions";
+    private static final String API_KEY = "78Kd7bqZLyRKpUbVoxSSnSzmzq2lM6IrGTTaXACW";
 
-        public static void main(String[] args) {
-            System.out.println("Hallo");
+
+        public static void main(String[] args) throws Exception {
+            HttpClient client = HttpClient.newHttpClient();
+            String url = API_URL + "?apiKey=" + API_KEY + "&limit=10";
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body());
 
         }
 }
