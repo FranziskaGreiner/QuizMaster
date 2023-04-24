a441da5ead7ca7136b1e175140b5dff0687d1390
Franziska Greiner <franziska.greiner98@gmx.de>
2023-04-24 16:22:15 +0200

    Add question controller, entity and repository

--- src/main/java/QuizMaster.java
+++ src/main/java/QuizMaster.java
@@ -1,26 +1,17 @@
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@SpringBootApplication
 public class QuizMaster {
 
-    private static final String API_URL = "https://quizapi.io/api/v1/questions";
-    private static final String API_KEY = "78Kd7bqZLyRKpUbVoxSSnSzmzq2lM6IrGTTaXACW";
+        public static void main(String[] args) {
 
-
-        public static void main(String[] args) throws Exception {
-            HttpClient client = HttpClient.newHttpClient();
-            String url = API_URL + "?apiKey=" + API_KEY + "&limit=10";
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(url))
-                    .GET()
-                    .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            System.out.println(response.body());
 
         }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
