084a0802ba543cc6d670a7f8d645e8277a897437
Franziska Greiner <franziska.greiner98@gmx.de>
2023-05-15 10:39:45 +0200

    Change app component and module

--- frontend/quizmaster-frontend/src/app/app.component.ts
+++ frontend/quizmaster-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {QuestionService} from "./question.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quizmaster-frontend';
+
+  constructor(public questionService: QuestionService) {
+  }
+
+  public getQuestions() {
+    this.questionService.findAll();
+  }
 }
