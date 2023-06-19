06a51a62a530b58778c7eed49b0fb9649e996573
Franziska Greiner <franziska.greiner98@gmx.de>
2023-06-19 21:00:05 +0200

    Add answer repository

---
+++ src/main/java/franziska/greiner/repository/AnswerRepository.java
@@ -0,0 +1,9 @@
+package franziska.greiner.repository;
+
+import franziska.greiner.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
