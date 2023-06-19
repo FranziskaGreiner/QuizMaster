2f0c77077f7a2cfc1e56bdd3ff870ce1b393ab88
Franziska Greiner <franziska.greiner98@gmx.de>
2023-06-19 21:00:28 +0200

    Add answer deserializer

---
+++ src/main/java/franziska/greiner/util/AnswersDeserializer.java
@@ -0,0 +1,30 @@
+package franziska.greiner.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import franziska.greiner.model.Answer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class AnswersDeserializer extends JsonDeserializer<List<Answer>> {
+    @Override
+    public List<Answer> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        List<Answer> answers = new ArrayList<>();
+
+        Map<String, String> answersMap = p.readValueAs(Map.class);
+
+        for (Map.Entry<String, String> entry : answersMap.entrySet()) {
+            if (entry.getValue() != null) {
+                Answer answer = new Answer();
+                answer.setAnswer(entry.getValue());
+                answers.add(answer);
+            }
+        }
+
+        return answers;
+    }
+}
