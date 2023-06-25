e909bd6aef2c0dd36df812fb1c4bbe969662b2ae
Franziska Greiner <franziska.greiner98@gmx.de>
2023-06-25 15:39:18 +0200

    Add quiz service

--- src/main/java/franziska/greiner/controller/QuizController.java
+++ src/main/java/franziska/greiner/controller/QuizController.java
@@ -1,13 +1,14 @@
 package franziska.greiner.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import franziska.greiner.model.Question;
+import franziska.greiner.service.QuizApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,15 +22,25 @@ public class QuizController {
     private static final int LIMIT = 10;
 
     @Autowired
+    private QuizApiService quizApiService;
+
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/quiz")
     public List<Question> getQuizQuestions() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        /*ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         String url = API_URL + "?apiKey=" + API_KEY + "&limit=" + LIMIT;
         String response = restTemplate.getForObject(url, String.class);
         System.out.println(response);
-        return objectMapper.readValue(response, new TypeReference<List<Question>>() {});
+        return objectMapper.readValue(response, new TypeReference<List<Question>>() {});*/
+        return quizApiService.fetchQuestions();
+    }
+
+    @PostMapping("/question")
+    public ResponseEntity<Void> storeQuestions() {
+        quizApiService.storeQuestions();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
---
+++ src/main/java/franziska/greiner/service/QuizApiService.java
@@ -0,0 +1,115 @@
+package franziska.greiner.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import franziska.greiner.model.Answer;
+import franziska.greiner.model.Question;
+import franziska.greiner.repository.AnswerRepository;
+import franziska.greiner.repository.QuestionRepository;
+import franziska.greiner.util.QuestionDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuizApiService {
+    private static final int LIMIT = 10;
+
+    @Autowired
+    private final WebClient webClient;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    public QuizApiService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<Question> fetchQuestions() {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/questions")
+                .queryParam("limit", LIMIT)
+                .build());
+        // Fetch as a String because WebClient does not directly support custom deserializers
+        Mono<String> responseMono = bodySpec.retrieve().bodyToMono(String.class);
+
+        // Convert String response to Question using ObjectMapper with custom deserializer
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Question.class, new QuestionDeserializer());
+        objectMapper.registerModule(module);
+
+        String response = responseMono.block();
+        List<Question> questionList = null;
+        try {
+            questionList = objectMapper.readValue(response, new TypeReference<List<Question>>(){});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return questionList;
+    }
+
+    public void storeQuestions() {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/questions")
+                .queryParam("limit", LIMIT)
+                .build());
+
+        // Fetch as a String because WebClient does not directly support custom deserializers
+        Mono<String> responseMono = bodySpec.retrieve().bodyToMono(String.class).log();
+
+        // Convert String response to ApiResponse using ObjectMapper and then to Question using the custom deserializer
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Question.class, new QuestionDeserializer());
+        objectMapper.registerModule(module);
+
+        String response = responseMono.block();
+        List<Question> questionList = null;
+        try {
+            questionList = objectMapper.readValue(response, new TypeReference<List<Question>>(){});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        if(questionList != null){
+            for (Question question : questionList) {
+                List<Answer> answerList = question.getAnswers();
+                question.setAnswers(new ArrayList<>());
+                Question insertedQuestion = questionRepository.save(question);
+                for (Answer answer : answerList) {
+                    answer.setQuestion(insertedQuestion);
+                }
+                answerRepository.saveAll(answerList);
+            }
+        }
+
+        /*Mono<List<Question>> response = bodySpec.retrieve().bodyToFlux(Question.class).collectList().log();
+        List<Question> questionList = response.block();
+        for (Question question : questionList) {
+            List<Answer> answerList = question.getAnswers();
+            question.setAnswers(new ArrayList<>());
+            Question insertedQuestion = questionRepository.save(question);
+            for (Answer answer : answerList) {
+                answer.setQuestion(insertedQuestion);
+            }
+            answerRepository.saveAll(answerList);
+        }*/
+    }
+}
