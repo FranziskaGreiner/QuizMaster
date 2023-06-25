f8c98aed155c54231a4e9f874c3e94e2b11b57da
Franziska Greiner <franziska.greiner98@gmx.de>
2023-06-25 15:42:51 +0200

    Fix cypress and jest configuration

--- frontend/quizmaster-frontend/jest.config.js
+++ frontend/quizmaster-frontend/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
 };
