97885b85dbfb4087a1c36bd188c04657324762ca
Franziska Greiner <franziska.greiner98@gmx.de>
2023-06-30 13:36:20 +0200

    Change waiting time in e2e test

--- frontend/quizmaster-frontend/cypress/e2e/app.cy.js
+++ frontend/quizmaster-frontend/cypress/e2e/app.cy.js
@@ -6,8 +6,8 @@ describe('Quiz App', () => {
 
     // check if questions are loaded and displayed
     cy.get('mat-card').should('be.visible')
-    // Wait up to 10 seconds for the 'mat-card-content' to be visible
-    cy.get('mat-card-content', { timeout: 10000 }).should('be.visible')
+    // Wait up to 20 seconds for the 'mat-card-content' to be visible
+    cy.get('mat-card-content', { timeout: 20000 }).should('be.visible')
 
     // check if each question has answers
     cy.get('mat-card-content').each(($card) => {
