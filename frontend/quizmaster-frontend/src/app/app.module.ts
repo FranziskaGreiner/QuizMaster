4f6bef3913c80655ec41539ea78a2f322048bc95
Franziska Greiner <franziska.greiner98@gmx.de>
2023-07-02 15:51:21 +0200

    Remove unused files in frontend

--- frontend/quizmaster-frontend/src/app/app.module.ts
+++ frontend/quizmaster-frontend/src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuizComponent } from './quiz/quiz.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatCheckboxModule} from "@angular/material/checkbox";
@@ -16,7 +15,6 @@ import {MatIconModule} from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
   ],
   imports: [
     BrowserModule,
