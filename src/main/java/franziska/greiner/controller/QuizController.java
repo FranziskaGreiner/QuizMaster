02b332c25d7e60153fd777521a546f9396ab9797
Franziska Greiner <franziska.greiner98@gmx.de>
2023-07-03 10:20:27 +0200

    Fix controller

--- src/main/java/franziska/greiner/controller/QuizController.java
+++ src/main/java/franziska/greiner/controller/QuizController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,7 +23,7 @@ public class QuizController {
         return quizApiService.fetchQuestions();
     }
 
-    @PostMapping("/question")
+    @GetMapping("/question")
     public ResponseEntity<Void> storeQuestions() {
         quizApiService.storeQuestions();
         return new ResponseEntity<>(HttpStatus.OK);
